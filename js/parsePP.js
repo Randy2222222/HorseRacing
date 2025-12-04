420ed609de71eda738a22d20b0acc6da0e3dc166
Randy2222222 <randyjpollitt@yahoo.com>
2025-12-04 17:46:51 -0500

    Update parsePP.js

--- js/parsePP.js
+++ js/parsePP.js
@@ -143,11 +143,15 @@ export function parsePP(decodedText) {
         currentPP.push(line);
 
         // distance
+       // const distMatch = line.match(DISTANCE_REGEX);
+       // if (distMatch) {
+      //    currentPPdistance = normalizeDistance(distMatch[0]);
+     //   }
+        // distance new
         const distMatch = line.match(DISTANCE_REGEX);
         if (distMatch) {
-          currentPPdistance = normalizeDistance(distMatch[0]);
+        currentPPdistance = distMatch[0];   // glyphMap already made it pretty
         }
-
         // surface
         const surfMatch = line.match(SURFACE_REGEX);
         if (surfMatch) {
