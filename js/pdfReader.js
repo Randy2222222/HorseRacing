5de9eab477f133236ec2adba7247c37e091e798c
Randy2222222 <randyjpollitt@yahoo.com>
2025-12-07 18:13:16 -0500

    Update pdfReader.js

--- js/pdfReader.js
+++ js/pdfReader.js
@@ -81,6 +81,7 @@ if (DEV_MODE === "structured") {
      // out += `    Race#: ${pp.race}\n`;
       out += `    Distance: ${pp.distance}\n`;
       out += `    Surface: ${pp.surface} (${pp.modifier || ""})\n`;
+      out += `    Condition: ${pp.condition}${pp.conditionSup || ""}\n`;
 
       out += `    Leader Times:\n`;
       out += `        1c: ${pp.leaderTimes.leader1.raw || ""} ${pp.leaderTimes.leader1.sup || ""}\n`;
