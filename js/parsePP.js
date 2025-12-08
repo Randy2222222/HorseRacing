7b543c78b51f0261677e31fc4a955f1a272a4630
Randy2222222 <randyjpollitt@yahoo.com>
2025-12-08 14:29:43 -0500

    Update parsePP.js

--- js/parsePP.js
+++ js/parsePP.js
@@ -35,7 +35,7 @@ const DISTANCE_REGEX =
   /\b([4-7](?:¹⁄₂)?f?|1m|2m|1m70|1(?:¹⁄₁₆|¹⁄₈|³⁄₁₆|¹⁄₄|³⁄₈|¹⁄₂|⁵⁄₈))\b/;
 
 // 3️⃣ Surface codes (2-letter)
-const SURFACE_CODES = [
+const SURFACE_REGEX = [
   "ft","gd","my","sy","wf","fm","yl","sf","hy","sl"
 ];
 
