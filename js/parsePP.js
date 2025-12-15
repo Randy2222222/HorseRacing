baa9224bb0d9c2baa706418970c0932db500e554
Randy2222222 <randyjpollitt@yahoo.com>
2025-12-15 14:06:01 -0500

    Update parsePP.js

--- js/parsePP.js
+++ js/parsePP.js
@@ -28,12 +28,12 @@ const DISTANCE_REGEX = /\b([4-7](?:½)?f?|1m|2m|1m70|1(?:¹⁄₁₆|⅛|³⁄�
 // 5️⃣ Surface codes (2-letter)
 //const SURFACE_REGEX = /\b(ft|gd|my|sy|wf|fm|yl|sf|hy|sl)([ˢˣⁿᵗʸ])?\b/i;
 //const SURFACE_REGEX = ["ft","gd","my","sy","wf","fm","yl","sf","hy","sl"];
-const SURFACE_REGEX = /(ft|gd|my|sy|wf|fm|yl|sf|hy|sl)/;
+//const SURFACE_REGEX = /(ft|gd|my|sy|wf|fm|yl|sf|hy|sl)/;
 
 const SURFACE_TAG_REGEX  =  /(s|x|n|t|y)/i;
 
 const SURFACE_TAG  =  ["s","x","n","t","y"];
-const SURFACE = ["ft","gd","my","sy","wf","fm","yl","sf","hy","sl"];
+//const SURFACE = ["ft","gd","my","sy","wf","fm","yl","sf","hy","sl"];
 const SURF_SUPS = ["ˢ","ˣ","ⁿ","ᵗ","ʸ"];
 
 //  6️⃣ Leader-time helper functions
@@ -281,7 +281,7 @@ if (!currentPPdistance && DISTANCE_REGEX.test(line)) {
  // continue;
 //}
 
-const SURFACE = trimmed.match(/(ft|gd|my|sy|wf|fm|yl|sf|hy|/);
+const SURFACE = trimmed.match(/(ft|gd|my|sy|wf|fm|yl|sf|hy)/i);
       if (SURFACE) {
         currentPPsuface = SURFACE[0];
         continue;
