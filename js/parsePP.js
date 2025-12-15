188a45514b6106b9b9969eb52744325073d80a42
Randy2222222 <randyjpollitt@yahoo.com>
2025-12-15 12:05:47 -0500

    Update parsePP.js

--- js/parsePP.js
+++ js/parsePP.js
@@ -29,6 +29,9 @@ const DISTANCE_REGEX = /\b([4-7](?:½)?f?|1m|2m|1m70|1(?:¹⁄₁₆|⅛|³⁄�
 //const SURFACE_REGEX = /\b(ft|gd|my|sy|wf|fm|yl|sf|hy|sl)([ˢˣⁿᵗʸ])?\b/i;
 //const SURFACE_REGEX = ["ft","gd","my","sy","wf","fm","yl","sf","hy","sl"];
 const SURFACE_REGEX = /\b(ft|gd|my|sy|wf|fm|yl|sf|hy|sl)$/;
+
+const SURFACE_TAG_REGEX  =  /(s|x|n|t|y)/i;
+
 const SURF_TAG  =  ["s","x","n","t","y"];
 const SURFACES = ["ft","gd","my","sy","wf","fm","yl","sf","hy","sl"];
 const SURF_SUPS = ["ˢ","ˣ","ⁿ","ᵗ","ʸ"];
