5ed9622ce91856052d54221119a6f5b6bbb9ba63
Randy2222222 <randyjpollitt@yahoo.com>
2025-12-17 19:30:56 -0500

    Update pdfReader.js

--- js/pdfReader.js
+++ js/pdfReader.js
@@ -105,6 +105,10 @@ if (DEV_MODE === "structured") {
       out += `    Post: ${pp.pp}\n`
       out += `    Start: ${pp.gate}\n`
       out += `   Lengths: ${pp.gatelng}\n`
+      out += `   1stCall: ${pp.first}\n`
+      out += `   2ndCall: ${pp.second}\n`
+      out += `   StrCall: ${pp.str}\n`
+      out += `   Finish: ${pp.finish}\n`
     });
 
     out += "========================================\n\n";
