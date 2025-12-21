ab88163aeb3d5bb9582f62dbe8f4aece4fe6d3eb
Randy2222222 <randyjpollitt@yahoo.com>
2025-12-20 22:50:57 -0500

    Fix regex for starting gate lengths to include whitespace

--- js/parsePP.js
+++ js/parsePP.js
@@ -69,7 +69,7 @@ const SPD_REGEX = /^\d{2,3}$/;   // matches 84 or 104
 const POST_POSITION_REGEX = /^\d{1,2}$/;
 const STARTING_GATE_REGEX = /^\d{1,2}$/;
 //const STARTING_GATE_LENGTHS_REGEX = /[⁰¹²³⁴⁵⁶⁷⁸⁹]{1,2}/;
-const STARTING_GATE_LENGTHS_REGEX = /s\*((?:¼|½|¾|)(?:[⁰¹²³⁴⁵⁶⁷⁸⁹]{1,2})(?:¼|½|¾|))/;
+const STARTING_GATE_LENGTHS_REGEX = /\s*((?:¼|½|¾|)(?:[⁰¹²³⁴⁵⁶⁷⁸⁹]{1,2})(?:¼|½|¾|))/;
 
 //const STARTING_GATE_LENGTHS_REGEX = /[\s\u00A0]*[⁰¹²³⁴⁵⁶⁷⁸⁹](?:¼|½|¾)?/;
 const FIRST_CALL_REGEX = /^\d{1,2}$/;
