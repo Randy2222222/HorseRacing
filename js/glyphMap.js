f5b10828f4a48e8f0a76a571b974a022230865aa
Randy2222222 <randyjpollitt@yahoo.com>
2025-12-23 18:37:14 -0500

    Comment out superscript glyph mappings
    Comment out glyph mappings for superscripts.

--- js/glyphMap.js
+++ js/glyphMap.js
@@ -65,9 +65,12 @@ export const GLYPHS = {
  // "\u0081": "½",
  // "ƒ": "¾",
 // Nose / Head / Neck display
-"¹": "ⁿˢ",
-"²": "ʰᵈ",
-"³": "ⁿᵏ",
+//"¹":
+"ⁿˢ"
+//"²": 
+"ʰᵈ"
+//"³":  
+"ⁿᵏ"
 //(?:ⁿˢ|ʰᵈ|ⁿᵏ)
 // Matches single superscripts or your racing abbreviations
 
