0e17fcffebfbf0b8382027656cfea756385d8651
Randy2222222 <randyjpollitt@yahoo.com>
2025-12-23 23:42:30 -0500

    Update regex patterns for starting gates and lengths
    Refined regex patterns for starting gates and lengths.

--- js/parsePP.js
+++ js/parsePP.js
@@ -62,19 +62,19 @@ const SHAPE_REGEX = /^[+-]?\d{1,3}$/;
 const SPD_REGEX = /^\d{2,3}$/;   // matches 84 or 104
 const POST_POSITION_REGEX = /^\d{1,2}$/;
 const STARTING_GATE_REGEX = /^\d{1,2}$/;
-const STARTING_GATE_LG_REGEX = /^((*:[⁰¹²³⁴⁵⁶⁷⁸⁹])(?:¼|½|¾|))$/;
+const STARTING_GATE_LG_REGEX = /^((?:[⁰¹²³⁴⁵⁶⁷⁸⁹]{1,2})(?:¼|½|¾|))$/;
 //const FIRST_LG_REGEX = /[⁰¹²³⁴⁵⁶⁷⁸⁹]{1,2}/;
 //const FIRST_LG_REGEX = /((?:¼|½|¾|)(?:[⁰¹²³⁴⁵⁶⁷⁸⁹]{1,2})(?:¼|½|¾|))/;
 
 //const STARTING_GATE_LENGTHS_REGEX = /[\s\u00A0]*[⁰¹²³⁴⁵⁶⁷⁸⁹](?:¼|½|¾)?/;
 const FIRST_CALL_REGEX = /^\d{1,2}$/;
-const FIRST_LG_REGEX = /^((*:[⁰¹²³⁴⁵⁶⁷⁸⁹])(?:¼|½|¾|))$/;
+const FIRST_LG_REGEX = /^((?:[⁰¹²³⁴⁵⁶⁷⁸⁹]{1,2})(?:¼|½|¾|))$/;
 const SECOND_CALL_REGEX = /^\d{1,2}$/;
-const SECOND_LG_REGEX = /^((*:[⁰¹²³⁴⁵⁶⁷⁸⁹])(?:¼|½|¾|))$/;
+const SECOND_LG_REGEX = /^((?:[⁰¹²³⁴⁵⁶⁷⁸⁹]{1,2}))(?:¼|½|¾|))$/;
 const STRAIGHT_CALL_REGEX = /^\d{1,2}$/;
-const STRAIGHT_LG_REGEX = /^((*:[⁰¹²³⁴⁵⁶⁷⁸⁹])(?:¼|½|¾|))$/;
+const STRAIGHT_LG_REGEX = /^((?:[⁰¹²³⁴⁵⁶⁷⁸⁹]{1,2}))(?:¼|½|¾|))$/;
 const FINISH_REGEX = /^\d{1,2}$/;
-const FINISH_LG_REGEX = /^((*:[⁰¹²³⁴⁵⁶⁷⁸⁹])(?:¼|½|¾|))$/;
+const FINISH_LG_REGEX = /^((?:[⁰¹²³⁴⁵⁶⁷⁸⁹]{1,2})(?:¼|½|¾|))$/;
 const JOCKEY_REGEX = /^[A-Z][a-z]+[A-Z]{1,2}[⁰¹²³⁴⁵⁶⁷⁸⁹]{2,3}$/;
 // Change SurfTag to Superscript
 const SUP_TAG = {
