414893e88c09517fbd23ed3fc39c5be71fa4d617
Randy2222222 <randyjpollitt@yahoo.com>
2025-12-26 00:16:38 -0500

    Update pdfReader.js

--- js/pdfReader.js
+++ js/pdfReader.js
@@ -6,7 +6,7 @@
 import { parsePP } from "./parsePP.js";
 import { applyGlyphMap } from "./glyphMap.js";
 
-const DEV_MODE = "structured";  // turn off later when finished
+const DEV_MODE = "decoded";  // turn off later when finished
 
 function updateStatus(msg) {
   document.getElementById("pdfStatus").textContent = msg;
