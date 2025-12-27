9fa4287b11c1a54a99c47a83a94402ff9ffad8fc
Randy2222222 <randyjpollitt@yahoo.com>
2025-12-27 05:02:05 -0500

    Fix finishers output in pdfReader.js

--- js/pdfReader.js
+++ js/pdfReader.js
@@ -112,7 +112,7 @@ if (DEV_MODE === "structured") {
       out += `   Jockey#: ${pp.jockey}\n`
       out += ` Equipment: ${pp.equipment}\n`
       out += `      Odds: ${pp.odds}\n`
-      out += ` Finishers: ${pp.win.wn}${pp.win.lg}${pp.place.pl}${pp.place.lg}${pp.show.sh}${pp.show.lg}\n`
+      out += ` Finishers: ${pp.win.wn}${pp.win.lg}${pp.place.pl}${pp.place.lg}${pp.last.sh}${pp.last.lg}\n`
    //   out += `  Comments: ${pp.comments}\n`
      // out += `     Field: ${pp.field}\n`
     });
